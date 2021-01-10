4d6d3ad8ecde8492e39f9dac71184f809f4d8644
Garg <ankita.garg@siemens.com>
2021-01-10 16:45:16 +0530

    Complete code at once

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WorkPerformedDelegate del1 = new WorkPerformedDelegate(WorkPerformed1);
+            //WorkPerformedDelegate del2 = new WorkPerformedDelegate(WorkPerformed2);
+
+            //del1(5, WorkType.GoToMeetings);
+            //del2(10, WorkType.GenerateReports);
+
+            var worker = new Worker();
+            worker.workPerformedEvent += Worker_WorkPerformed;
+            worker.WorkCompletedEvent += Worker_WorkCompleted;
+            worker.DoWork(8, WorkType.GoToMeetings);           
+        }
+
+        static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        {
+            Console.WriteLine("Hours worked : " + e.Hours + " on work : " + e.WorkType);
+        }
+
+        static void Worker_WorkCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Work comleted");
+        }
+
+        //static void WorkPerformed1(int hours, WorkType workType)
+        //{
+        //    Console.WriteLine("Work performed1 called : " +hours.ToString());
+        //}
+
+        //static void WorkPerformed2(int hours, WorkType workType)
+        //{
+        //    Console.WriteLine("Work performed2 called : " + hours.ToString());
+        //}
+    }
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        GenerateReports,
+        FillTimesheet
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    //public delegate void WorkPerformedDelegate(int hours, WorkType workType);
+
+    public class Worker
+    {
+        public event EventHandler<WorkPerformedEventArgs> workPerformedEvent;
+        public event EventHandler WorkCompletedEvent;
+
+        public virtual void DoWork(int hours, WorkType workType)
+        {
+            //Do work here and notify the customer that the work has been performed
+            for (int i = 0; i < hours; i++)
+            {
+                OnWorkPerformed(i + 1, workType);
+            }
+            OnWorkCompleted();
+        }
+
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+            // workPerformedEvent?.Invoke(hours, workType);   // raising event via event
+            var del = workPerformedEvent as EventHandler<WorkPerformedEventArgs>;  // casting the event as a delegate as the data type of event is delegate
+            del?.Invoke(this, new WorkPerformedEventArgs(hours, workType)); // raising event via delegate
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            EventHandler del = WorkCompletedEvent as EventHandler;
+            del?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
